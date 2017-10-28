466e85845f1ec6de33468e30501285d62d74fe93
Monica  Karasek <mlkarasek@gmail.com>
2017-10-28 01:02:17 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,5 +1,4 @@
 class Genre
-
   attr_accessor :name
 
   def initialize(name)
@@ -7,24 +6,17 @@ class Genre
     @songs = []
   end
 
+  def songs
+    @songs
+  end
+  
   def add_song(song)
     @songs << song
   end
 
-  def songs
-    @songs
-  end
-
   def artists
     @songs.collect do |song|
-      song.artist = self
+      song.artist
+    end
   end
-end
-
-def genres
-  self.songs.collect do |song|
-    song.genre
-  end
-end 
-
 end
