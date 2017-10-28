80cbd53b7be5f9e1d60806e6cf7f33d54e148aca
Monica  Karasek <mlkarasek@gmail.com>
2017-10-28 00:33:41 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,17 @@
+class Doctor 
+
+  attr_accessor :name, :appointments
+
+  def initialize(name)
+    @name = name 
+    @appointments = []
+  end 
+
+def appointments
+  @appointments
+end 
+
+  def doctor 
+    @doctor 
+  end 
+  
---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+
+  attr_accessor :name, :genre
+
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+  end
+
+  def add_song
+  end 
+
+end
